ed23cb08de8c37a078f16f7ed0f144b6be5e937b
Mikhail Nepomnyashchiy <36237995+michey85@users.noreply.github.com>
2019-07-16 02:38:30 +0300

    site files upload

---
+++ precache-manifest.c3914f56448e77c80cdf32fb13f11407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba77e9e61dd92e7b0637f210cb2a178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b280e4c13cf8e0c6adf",
+    "url": "/static/css/main.d108225d.chunk.css"
+  },
+  {
+    "revision": "5b76ac8b83b1f921e877",
+    "url": "/static/js/2.7bff1c4b.chunk.js"
+  },
+  {
+    "revision": "0b280e4c13cf8e0c6adf",
+    "url": "/static/js/main.a9894d2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d6ca90b6077e3f12a8d2f179f50a04a",
+    "url": "/static/media/duck.5d6ca90b.png"
+  }
+]);
